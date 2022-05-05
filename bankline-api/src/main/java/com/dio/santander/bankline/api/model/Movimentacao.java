b5a9cedf3c9e737425180fb446111f507420d309
Maria Luiza Moura <malumoura10@gmail.com>
2022-05-05 16:44:02 -0300

    Atualização do projeto, interação de usuário DTO

--- bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -19,11 +19,15 @@ public class Movimentacao {
 	private Integer id;
 	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
+
 	private String descricao;
 	private Double valor;
 
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
 
 	public Integer getId() {
 		return id;
@@ -65,4 +69,11 @@ public class Movimentacao {
 		this.tipo = tipo;
 	}
 
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
+	
 }
