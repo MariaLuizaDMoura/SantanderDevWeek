bb9e46e8c9188a7f3591a44c979fadc6ba917ab4
Maria Luiza Moura <malumoura10@gmail.com>
2022-05-05 11:38:36 -0300

    Mapeamento de entidades usando JPA

--- bankline-api/src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,10 +1,27 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista") //salva os registros da classe numa tabela chamada correntista
 public class Correntista {
+	@Id //chave primaria, identificador do registro
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //criar um achave unica para cada registro inserido
 	private Integer id;
+	@Column(length = 20)
 	private String cpf;
+	
+	@Column(length = 60)
 	private String nome;
 
+	
+	@Embedded //as informações que estiverem nessa classe estarão na mesma tabela que a tabela principal.
 	private Conta conta;
 	public Conta getConta() {
 		return conta;
